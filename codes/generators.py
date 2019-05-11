24cc9f591854ab23aafd8ba42c445c1b08a34a73
Mohammad Azhdari <mohammad.azhdari.22@gmail.com>
2019-05-11 19:42:11 +0430

    practice with corotines

--- codes/generators.py
+++ codes/generators.py
@@ -1,11 +1,22 @@
 import itertools
-import statistics
+# import statistics
 
 
 
 def process_purchases(purchases):
-	min_, max_, avg = itertools.tee(purchases, 3)
-	return min(min_), max(max_), statistics.median(avg)
+	# creates two copies of this list and pass it to variables.
+	min_, max_ = itertools.tee(purchases, 2)
+	return min(min_), max(max_)
 
 test_list = [1, 2, 3, 4]
 print(process_purchases(test_list))
+
+
+def test_corotin():
+	a = 0
+	while True:
+		a += 1
+		yield a
+
+for a in test_corotin():
+	print(a)
